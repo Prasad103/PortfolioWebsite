34f8bf416637aeb7378b1ebac00cbbe6079736d2
Prasad103 <prasadbebale01@gmail.com>
2023-12-19 19:38:54 +0530

    New

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import Header from './components/header/Header.jsx'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Project from './components/project/Project'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
     <>
         <Header />
+        <Nav />
+        <About />
+        <Project />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
     
     </>
   )
